de3846f5f87fce702253bdfb736b7ac9ef4caef2
Siddharth <officialsiddharthbisht@gmail.com>
2022-04-04 20:32:28 +0530

    started logic of expense tracker

---
+++ script.js
@@ -0,0 +1,6 @@
+function displayCard(){
+    const month = document.getElementById("month").value;
+    const income = document.getElementById("income").value;
+    const expense = document.getElementById("expense").value;
+    console.log(month , income ,expense ,"working");
+}
